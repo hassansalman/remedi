4a6171324f851f3581b60eaf3d0d1a72ba5b42d5
Hassan Salman <hassansalman8@gmail.com>
2016-08-19 15:52:39 -0700

    fixed hover link color

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,18 @@
 // --------------------------------------------------------
 // Pretty Photo for Lightbox Image
-// -------------------------------------------------------- 
-$(document).ready(function() {	
-    $("a[data-gal^='prettyPhoto']").prettyPhoto(); 
+// --------------------------------------------------------
+$(document).ready(function() {
+    $("a[data-gal^='prettyPhoto']").prettyPhoto();
 });
 
 // --------------------------------------------------------
 //	Navigation Bar
-// -------------------------------------------------------- 	
-$(window).scroll(function(){	
-	"use strict";	
+// --------------------------------------------------------
+$(window).scroll(function(){
+	"use strict";
 	var scroll = $(window).scrollTop();
-	if( scroll > 60 ){		
-		$(".navbar").addClass("scroll-fixed-navbar");				
+	if( scroll > 60 ){
+		$(".navbar").addClass("scroll-fixed-navbar");
 	} else {
 		$(".navbar").removeClass("scroll-fixed-navbar");
 	}
@@ -20,10 +20,44 @@ $(window).scroll(function(){
 
 // --------------------------------------------------------
 //	Smooth Scrolling
-// -------------------------------------------------------- 	
+// --------------------------------------------------------
 $(".navbar-nav li a[href^='#']").on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({
         scrollTop: $(this.hash).offset().top
     }, 1000);
+});
+
+// --------------------------------------------------------
+//	News Slider
+// --------------------------------------------------------
+
+$(document).ready(function(){
+
+	var clickEvent = false;
+	$('#myCarousel').carousel({
+		interval:   5000
+	}).on('click', '.list-group li', function() {
+			clickEvent = true;
+			$('.list-group li').removeClass('active');
+			$(this).addClass('active');
+	}).on('slid.bs.carousel', function(e) {
+		if(!clickEvent) {
+			var count = $('.list-group').children().length -1;
+			var current = $('.list-group li.active');
+			current.removeClass('active').next().addClass('active');
+			var id = parseInt(current.data('slide-to'));
+			if(count == id) {
+				$('.list-group li').first().addClass('active');
+			}
+		}
+		clickEvent = false;
+	});
+})
+
+$(window).load(function() {
+    var boxheight = $('#myCarousel .carousel-inner').innerHeight();
+    var itemlength = $('#myCarousel .item').length;
+    var triggerheight = Math.round(boxheight/itemlength+1);
+	$('#myCarousel .list-group-item').outerHeight(triggerheight);
 });
